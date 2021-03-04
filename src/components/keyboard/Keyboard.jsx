1e5a562ba7dbf53fbf392f835990d4470be62c03
CodeKrakken <newholm1@hotmail.com>
2021-03-04 13:18:03 +0000

    Respond to individual keys with alerts

--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 function Keyboard({ play, stop }) {
   return (
     <div>
+
       <button
         onKeyDown={onKeyDown}
         onKeyUp={onKeyUp}
       >
         C4
       </button>
+      <input type="text" onKeyDown={myFunction} />
+
     </div>
   )
 
@@ -18,6 +21,16 @@ function Keyboard({ play, stop }) {
 
   function onKeyUp() {
     stop()
+  }
+
+  function myFunction(event) {
+    var x = event.keyCode;
+    if (x === 27) {  // 27 is the ESC key
+      alert ("You pressed the Escape key!");
+    }
+    if (x === 90) {
+      alert ('Z')
+    }
   }
 }
 
