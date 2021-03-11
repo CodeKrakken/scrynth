de1b6afda742e1d284419a181925e67e05b4cb27
CodeKrakken <newholm1@hotmail.com>
2021-03-11 14:02:45 +0000

    Remove useEffect

--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Synth from '../synth/Synth';
 const synth = new Synth()
 
 function Keyboard() {
 
-  useEffect(() => {
+  // useEffect(() => {
     function handleKeyDown(e) {
       synth.play(e.keyCode)
       console.log(e.keyCode)
@@ -18,11 +18,11 @@ function Keyboard() {
     document.addEventListener('keyup', handleKeyUp);
 
     // Don't forget to clean up
-    return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-      document.removeEventListener('keyup', handleKeyUp);
-    }
-  }, []);
+    // return function cleanup() {
+    //   document.removeEventListener('keydown', handleKeyDown);
+    //   document.removeEventListener('keyup', handleKeyUp);
+    // }
+  // }, []);
 
   return <div>KEYBOARD</div>;
 
