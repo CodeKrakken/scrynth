7988f4905a8721372d116e106490474229a439b1
CodeKrakken <newholm1@hotmail.com>
2021-03-11 14:41:42 +0000

    Add octave codes to keyboard component

--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -3,25 +3,44 @@ import Synth from '../synth/Synth';
 const synth = new Synth()
 
 function Keyboard() {
-  const keyCodes = {
-    90: 'C4',
-    83: 'C#4',
-    88: 'D4',
-    68: 'D#4',
-    67: 'E4',
-    86: 'F4',
-    71: 'F#4',
-    66: 'G4',
-    72: 'G#4',
-    78: 'A4',
-    74: 'A#4',
-    77: 'B4',
-    188: 'C5'
+  const noteCodes = {
+    90: 'C',
+    83: 'C#',
+    88: 'D',
+    68: 'D#',
+    67: 'E',
+    86: 'F',
+    71: 'F#',
+    66: 'G',
+    72: 'G#',
+    78: 'A',
+    74: 'A#',
+    77: 'B',
+    188: 'C'
+  }
+
+  const octaveCodes = {
+    192: 0,
+    49: 1,
+    50: 2,
+    51: 3,
+    52: 4,
+    53: 5,
+    54: 6,
+    55: 7,
+    56: 8,
+    57: 9,
+    48: 10
   }
 
   function handleKeyDown(e) {
-    if (e.keyCode in keyCodes) {
-      synth.play(keyCodes[e.keyCode])
+    console.log(e)
+    if (e.keyCode in noteCodes) {
+      synth.play(noteCodes[e.keyCode])
+    }
+
+    if (e.keyCode in octaveCodes) {
+      synth.octave(octaveCodes[e.keyCode])
     }
   }
 
