f2e85b1ec67050183f95699dff20355a6d7ccb23
CodeKrakken <newholm1@hotmail.com>
2023-01-15 13:07:24 +0000

    Make dark mode native

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/header/Header';
+import Keyboard from './components/keyboard/Keyboard'
+
+export default function Scrynth() {
+  
+  return <>
+    <Header />
+    <Keyboard />
+  </>
+}
